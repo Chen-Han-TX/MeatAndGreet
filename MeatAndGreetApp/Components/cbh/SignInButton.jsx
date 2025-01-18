3d9698075a8b583156d69deab72bac2eba79adb3
Boon Hsuan Chua <puppylongears@gmail.com>
2025-01-18 16:27:06 +0800

    idk

---
+++ MeatAndGreetApp/Components/cbh/SignInButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Button } from 'react-native';
+
+function GoogleSignIn() {
+    return (
+        <Button
+            title="Google Sign-In"
+            onPress={() => onGoogleButtonPress().then(() => console.log('Signed in with Google!'))}
+        />
+    );
+}
